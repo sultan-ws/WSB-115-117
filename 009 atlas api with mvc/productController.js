40b16a1926fe6000dd09c95fa0ab7e132263cabb
sultanBytes <sultankhand11@gmail.com>
2024-10-18 11:59:40 +0530

    update and delete api's

--- 009 atlas api with mvc/productController.js
+++ 009 atlas api with mvc/productController.js
@@ -1,46 +1,119 @@
 const Product = require("./productModel");
+const fs = require('fs');
 
-const createProduct = async (req, res)=>{
-    try{
+const createProduct = async (req, res) => {
+    try {
         const data = req.body;
 
-        if(req.files){
-            if(req.files.thumbnail) data.thumbnail = req.files.thumbnail[0].filename;
+        if (req.files) {
+            if (req.files.thumbnail) data.thumbnail = req.files.thumbnail[0].filename;
 
-            if(req.files.images) data.images = req.files.images.map((img)=> img.filename);
+            if (req.files.images) data.images = req.files.images.map((img) => img.filename);
         }
 
         const dataToSave = new Product(data);
 
         const response = await dataToSave.save();
 
-        res.status(200).json({message: 'success', data: response});
+        res.status(200).json({ message: 'success', data: response });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(500).json({message: 'internal server error'});
+        res.status(500).json({ message: 'internal server error' });
     }
 };
 
-const readProducts = async (req, res) =>{
-    try{
+const readProducts = async (req, res) => {
+    try {
         const data = await Product.find();
         const filepath = `${req.protocol}://${req.get('host')}/web-files/`;
 
-        const dummyArray = data.map((product)=>{
+        const dummyArray = data.map((product) => {
             product.thumbnail = filepath + product.thumbnail;
 
-            product.images = product.images.map((img)=> filepath + img);
+            product.images = product.images.map((img) => filepath + img);
 
             return product;
         });
 
-        res.status(200).json({message: 'success', data: dummyArray, filepath});
+        res.status(200).json({ message: 'success', data: dummyArray, filepath });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(500).json({message: 'internal server error'});
+        res.status(500).json({ message: 'internal server error' });
     }
 }
 
-module.exports = {createProduct, readProducts}
+const updateProduct = async (req, res) => {
+    try {
+        const ifProduct = await Product.findOne(req.params);
+
+        if (!ifProduct) return res.status(400).json({ message: 'data not found' });
+
+        console.log(ifProduct);
+
+        const data = req.body;
+
+        if (req.files) {
+            if (req.files.thumbnail) {
+                data.thumbnail = req.files.thumbnail[0].filename;
+
+                console.log(fs.existsSync(`./uploads/products/${ifProduct.thumbnail}`));
+                if (fs.existsSync(`./uploads/products/${ifProduct.thumbnail}`)) {
+                    fs.unlinkSync(`./uploads/products/${ifProduct.thumbnail}`)
+                }
+            }
+
+            if (req.files.images) {
+                data.images = req.files.images.map((img) => img.filename);
+
+                ifProduct.images.map((img) => {
+                    if (fs.existsSync(`./uploads/products/${img}`)) {
+                        fs.unlinkSync(`./uploads/products/${img}`)
+                    }
+                })
+
+            }
+        }
+        const response = await Product.updateOne(
+            req.params,
+            {
+                $set: data
+            }
+        );
+
+        res.status(200).json({ message: 'success', data: response });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'internal server error' });
+    }
+}
+
+
+const deleteProduct = async (req, res) => {
+    try {
+        const ifProduct = await Product.findOne(req.params);
+
+        if (!ifProduct) return res.status(400).json({ message: 'data not found' });
+
+        if (fs.existsSync(`./uploads/products/${ifProduct.thumbnail}`)) {
+            fs.unlinkSync(`./uploads/products/${ifProduct.thumbnail}`)
+        }
+
+        ifProduct.images.map((img) => {
+            if (fs.existsSync(`./uploads/products/${img}`)) {
+                fs.unlinkSync(`./uploads/products/${img}`)
+            }
+        });
+
+        const response = await Product.deleteOne(req.params);
+        res.status(200).json({message: 'success', data: response});
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'internal server error' });
+    }
+}
+    module.exports = { createProduct, readProducts, updateProduct, deleteProduct }
--- 009 atlas api with mvc/productRoutes.js
+++ 009 atlas api with mvc/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const upload = require('./multer');
-const {createProduct, readProducts} = require('./productController');
+const {createProduct, readProducts, updateProduct, deleteProduct} = require('./productController');
 
 const productRoutes = express.Router();
 
 productRoutes.post('/insert-data', upload('products'), createProduct);
 productRoutes.get('/read-products', readProducts);
+productRoutes.put('/update-products/:_id',upload('products'), updateProduct);
+productRoutes.delete('/delete-product/:_id', deleteProduct);
 
 module.exports = productRoutes;
